32580a6ef41e289b70dbf029f18d9631e8072593
13764450022 <zhujunhua@saicmotor.com>
2019-01-09 14:09:19 +0800

    fix unicode error

--- spider/runner/extractor.py
+++ spider/runner/extractor.py
@@ -1,4 +1,5 @@
 import json
+import traceback
 
 from redis.client import Redis
 from pymongo.database import Database
@@ -34,10 +35,15 @@ def run_keyword(r: Redis, db: Database):
     nlp = Nlp()
     while r.exists(KEYWORD_KEY):
         article = json.loads(r.lpop(KEYWORD_KEY).decode())
+        url = article.get("url")
         title = article.get("title", "")
         content = article.get("content", "")
-        keywords = nlp.keyword(title, content)
-        if article.get("url"):
-            save_article({"url": article.get("url"), "keyword": keywords})
-            logging.info("update keyword success. keyword: {}".format(keywords))
+        if url:
+            try:
+                keywords = nlp.keyword(title, content)
+                save_article({"url": url, "keyword": keywords})
+                logging.info("update keyword success. keyword: {}".format(keywords))
+            except UnicodeEncodeError:
+                traceback.print_exc()
+                save_article({"url": url})
     logging.info("job complete.")
