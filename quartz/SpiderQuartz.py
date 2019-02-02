c6ecd6c5e6ff844c86984a707426308796936946
13764450022 <zhujunhua@saicmotor.com>
2019-02-02 16:08:38 +0800

    cron

--- quartz/SpiderQuartz.py
+++ quartz/SpiderQuartz.py
@@ -5,7 +5,8 @@ from script import spider_script
 class SpiderScheduler(Quartz):
 
     def config(self):
-        self.schedudler.add_job(spider_script.reset_stock, "cron", hour="*/3")
+        self.schedudler.add_job(spider_script.reset_topic, "cron", hour="16", minute="08")
+        self.schedudler.add_job(spider_script.run_article, "cron", hour="16", minute="09")
 
 
 if __name__ == '__main__':
